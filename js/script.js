dbdd8b3f3e01b074086408ab0a55b2aded0bfe54
josegonzalo <gonzaloja.1000@gmail.com>
2019-08-31 11:03:50 -0600

    La inspiracion

--- js/script.js
+++ js/script.js
@@ -1,52 +1,81 @@
- var firebaseConfig = {
-    apiKey: "AIzaSyBWaIas-guCZ9IGjCdMfbkVJNoOSpTtRz8",
-    authDomain: "maxapp-659bd.firebaseapp.com",
-    databaseURL: "https://maxapp-659bd.firebaseio.com",
-    projectId: "maxapp-659bd",
-    storageBucket: "maxapp-659bd.appspot.com",
-    messagingSenderId: "537653727870",
-    appId: "1:537653727870:web:9466a59097b32b88"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+var firebaseConfig = {
+	apiKey: "AIzaSyBWaIas-guCZ9IGjCdMfbkVJNoOSpTtRz8",
+	authDomain: "maxapp-659bd.firebaseapp.com",
+	databaseURL: "https://maxapp-659bd.firebaseio.com",
+	projectId: "maxapp-659bd",
+	storageBucket: "maxapp-659bd.appspot.com",
+	messagingSenderId: "537653727870",
+	appId: "1:537653727870:web:9466a59097b32b88"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-    var storage = firebase.storage();
-  
+
+  // Get a non-default Storage bucket
+  var storage = firebase.app().storage("gs://maxapp-659bd.appspot.com");
+	var file;
+
+
 var max = angular.module("max", [])
 	max.controller("productoCTRL", function ($scope){
 			$scope.Producto = {}
+
+
+
+
+
+
+
 		$scope.cargarproductos = function(x){
 			$scope.Producto = x
-			firebase.database().ref("/Producto").push($scope.Producto)
+
+			storage.ref(file.name).put(file).then(function(snapshot) {
+			//console.log('Uploaded a blob or file!');
+
+				var prodRef = storage.ref(file.name);
+
+				prodRef.getDownloadURL().then(function(url) {
+
+					$scope.Producto["foto"] = url;
+					
+					firebase.database().ref("/Producto").push($scope.Producto);
+
+
+				})
+
+
+
+			});
+
+
+			console.log(file.result)
+			//firebase.database().ref("/Producto").push($scope.Producto)
 		}
 	})
+//Muestra la imagen de en la ventana de productos
+	function archivo(evt) {
+		  var files = evt.target.files; // FileList object
+		  //Obtenemos la imagen del campo "file". 
+		  for (var i = 0, f; f = files[i]; i++) {
+				fileName = files[i];
 
-function archivo(evt) {
-      var files = evt.target.files; // FileList object
-       
-        //Obtenemos la imagen del campo "file". 
-      for (var i = 0, f; f = files[i]; i++) {         
-           //Solo admitimos imágenes.
-           if (!f.type.match('image.*')) {
-                continue;
-           }
-       
-           var reader = new FileReader();
-           
-           reader.onload = (function(theFile) {
-               return function(e) {
-               // Creamos la imagen.
-                      document.getElementById("list").innerHTML = ['<img class="thumb" src="', e.target.result,'" title="', escape(theFile.name), '"/>'].join('');
-               };
-           })(f);
- 
-           reader.readAsDataURL(f);
-       }
-}
-             
-      document.getElementById('files').addEventListener('change', archivo, false);
 
-/*function reloadThePage(){
-    window.location.reload();
-} 
-*/
+			   //Solo admitimos imágenes.
+			   if (!f.type.match('image.*')) {
+					continue;
+			   }
+			   var reader = new FileReader();
+			   
+			   reader.onload = (function(theFile) {
+				   return function(e) {
+				   // Creamos la imagen.
+						  document.getElementById("list").innerHTML = ['<img class="thumb" src="', e.target.result,'" title="', escape(theFile.name), '"/>'].join('');
+				   };
+			   })(f);
+	 
+			   reader.readAsDataURL(f);
+		   }
+		   file = files[0];
+	}
+			 
+	  document.getElementById('files').addEventListener('change', archivo, false);
